9e54abae4798a53987a1d71f79e36ae9e016781b
Anton Schirg <anton.schirg@posteo.de>
2015-02-02 17:30:12 +0100

    doesnt start gui anymore

--- src/main.c
+++ src/main.c
@@ -119,184 +119,19 @@ void init_stuff (int argc, char *argv[])
   ui.cur_mapping = 0;
   ui.which_unswitch_button = 0;
 
-  reset_recognizer();
-
-  // initialize various interface elements
-  
-  gtk_window_set_default_size(GTK_WINDOW (winMain), ui.window_default_width, ui.window_default_height);
-  if (ui.maximize_at_start) gtk_window_maximize(GTK_WINDOW (winMain));
-  update_toolbar_and_menu();
-  update_font_button();
-
-  gtk_check_menu_item_set_active(
-    GTK_CHECK_MENU_ITEM(GET_COMPONENT("journalApplyAllPages")), ui.bg_apply_all_pages);
-  if (ui.fullscreen) {
-    gtk_check_menu_item_set_active(
-      GTK_CHECK_MENU_ITEM(GET_COMPONENT("viewFullscreen")), TRUE);
-    gtk_toggle_tool_button_set_active(
-      GTK_TOGGLE_TOOL_BUTTON(GET_COMPONENT("buttonFullscreen")), TRUE);
-    gtk_window_fullscreen(GTK_WINDOW(winMain));
-  }
-  gtk_button_set_relief(GTK_BUTTON(GET_COMPONENT("buttonColorChooser")), GTK_RELIEF_NONE);
-
-  allow_all_accels();
-  add_scroll_bindings();
-
-  // prevent interface items from stealing focus
-  // glade doesn't properly handle can_focus, so manually set it
-  gtk_combo_box_set_focus_on_click(GTK_COMBO_BOX(GET_COMPONENT("comboLayer")), FALSE);
-  g_signal_connect(GET_COMPONENT("spinPageNo"), "activate",
-          G_CALLBACK(handle_activate_signal), NULL);
-  gtk_container_forall(GTK_CONTAINER(winMain), unset_flags, (gpointer)GTK_CAN_FOCUS);
-  GTK_WIDGET_SET_FLAGS(GTK_WIDGET(canvas), GTK_CAN_FOCUS);
-  GTK_WIDGET_SET_FLAGS(GTK_WIDGET(GET_COMPONENT("spinPageNo")), GTK_CAN_FOCUS);
-  
-  // install hooks on button/key/activation events to make the spinPageNo lose focus
-  gtk_container_forall(GTK_CONTAINER(winMain), install_focus_hooks, NULL);
-
-  // set up and initialize the canvas
-
-  gtk_widget_show (GTK_WIDGET (canvas));
-  w = GET_COMPONENT("scrolledwindowMain");
-  gtk_container_add (GTK_CONTAINER (w), GTK_WIDGET (canvas));
-  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW (w), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-  gtk_widget_set_events (GTK_WIDGET (canvas), GDK_EXPOSURE_MASK | GDK_POINTER_MOTION_MASK | GDK_BUTTON_MOTION_MASK | GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK | GDK_KEY_PRESS_MASK | GDK_ENTER_NOTIFY_MASK | GDK_LEAVE_NOTIFY_MASK);
-  gnome_canvas_set_pixels_per_unit (canvas, ui.zoom);
-  gnome_canvas_set_center_scroll_region (canvas, TRUE);
-  gtk_layout_get_hadjustment(GTK_LAYOUT (canvas))->step_increment = ui.scrollbar_step_increment;
-  gtk_layout_get_vadjustment(GTK_LAYOUT (canvas))->step_increment = ui.scrollbar_step_increment;
-
   // set up the page size and canvas size
   update_page_stuff();
-
-  g_signal_connect ((gpointer) canvas, "button_press_event",
-                    G_CALLBACK (on_canvas_button_press_event),
-                    NULL);
-  g_signal_connect ((gpointer) canvas, "button_release_event",
-                    G_CALLBACK (on_canvas_button_release_event),
-                    NULL);
-  g_signal_connect ((gpointer) canvas, "enter_notify_event",
-                    G_CALLBACK (on_canvas_enter_notify_event),
-                    NULL);
-  g_signal_connect ((gpointer) canvas, "leave_notify_event",
-                    G_CALLBACK (on_canvas_leave_notify_event),
-                    NULL);
-  g_signal_connect ((gpointer) canvas, "expose_event",
-                    G_CALLBACK (on_canvas_expose_event),
-                    NULL);
-  g_signal_connect ((gpointer) canvas, "key_press_event",
-                    G_CALLBACK (on_canvas_key_press_event),
-                    NULL);
-  g_signal_connect ((gpointer) canvas, "motion_notify_event",
-                    G_CALLBACK (on_canvas_motion_notify_event),
-                    NULL);
-  g_signal_connect ((gpointer) gtk_layout_get_vadjustment(GTK_LAYOUT(canvas)),
-                    "value-changed", G_CALLBACK (on_vscroll_changed),
-                    NULL);
-  g_signal_connect ((gpointer) gtk_layout_get_hadjustment(GTK_LAYOUT(canvas)),
-                    "value-changed", G_CALLBACK (on_hscroll_changed),
-                    NULL);
   g_object_set_data (G_OBJECT (winMain), "canvas", canvas);
 
   screen = gtk_widget_get_screen(winMain);
   ui.screen_width = gdk_screen_get_width(screen);
   ui.screen_height = gdk_screen_get_height(screen);
   
-  can_xinput = FALSE;
-  dev_list = gdk_devices_list();
-  while (dev_list != NULL) {
-    device = (GdkDevice *)dev_list->data;
-    if (device != gdk_device_get_core_pointer() && device->num_axes >= 2) {
-      /* get around a GDK bug: map the valuator range CORRECTLY to [0,1] */
-#ifdef ENABLE_XINPUT_BUGFIX
-      gdk_device_set_axis_use(device, 0, GDK_AXIS_IGNORE);
-      gdk_device_set_axis_use(device, 1, GDK_AXIS_IGNORE);
-#endif
-      gdk_device_set_mode(device, GDK_MODE_SCREEN);
-      if (g_strrstr(device->name, "raser"))
-        gdk_device_set_source(device, GDK_SOURCE_ERASER);
-      can_xinput = TRUE;
-    }
-    dev_list = dev_list->next;
-  }
-  if (!can_xinput)
-    gtk_widget_set_sensitive(GET_COMPONENT("optionsUseXInput"), FALSE);
-
-  ui.use_xinput = ui.allow_xinput && can_xinput;
-
-  gtk_check_menu_item_set_active(
-    GTK_CHECK_MENU_ITEM(GET_COMPONENT("optionsProgressiveBG")), ui.progressive_bg);
-  gtk_check_menu_item_set_active(
-    GTK_CHECK_MENU_ITEM(GET_COMPONENT("optionsPrintRuling")), ui.print_ruling);
-  gtk_check_menu_item_set_active(
-    GTK_CHECK_MENU_ITEM(GET_COMPONENT("optionsAutoloadPdfXoj")), ui.autoload_pdf_xoj);
-  gtk_check_menu_item_set_active(
-    GTK_CHECK_MENU_ITEM(GET_COMPONENT("optionsLeftHanded")), ui.left_handed);
-  gtk_check_menu_item_set_active(
-    GTK_CHECK_MENU_ITEM(GET_COMPONENT("optionsShortenMenus")), ui.shorten_menus);
-  gtk_check_menu_item_set_active(
-    GTK_CHECK_MENU_ITEM(GET_COMPONENT("optionsAutoSavePrefs")), ui.auto_save_prefs);
-  gtk_check_menu_item_set_active(
-    GTK_CHECK_MENU_ITEM(GET_COMPONENT("optionsButtonSwitchMapping")), ui.button_switch_mapping);
-  gtk_check_menu_item_set_active(
-    GTK_CHECK_MENU_ITEM(GET_COMPONENT("optionsPenCursor")), ui.pen_cursor);
-  
-  hide_unimplemented();
-
-  update_undo_redo_enabled();
-  update_copy_paste_enabled();
-  update_vbox_order(ui.vertical_order[ui.fullscreen?1:0]);
-  gtk_widget_grab_focus(GTK_WIDGET(canvas));
-
-  // show everything...
-  
-  gtk_widget_show (winMain);
-  update_cursor();
-
-  /* this will cause extension events to get enabled/disabled, but
-     we need the windows to be mapped first */
-  gtk_check_menu_item_set_active(
-    GTK_CHECK_MENU_ITEM(GET_COMPONENT("optionsUseXInput")), ui.use_xinput);
-
-  /* fix a bug in GTK+ 2.16 and 2.17: scrollbars shouldn't get extended
-     input events from pointer motion when cursor moves into main window */
-
-  if (!gtk_check_version(2, 16, 0)) {
-    g_signal_connect (
-      GET_COMPONENT("menubar"),
-      "event", G_CALLBACK (filter_extended_events),
-      NULL);
-    g_signal_connect (
-      GET_COMPONENT("toolbarMain"),
-      "event", G_CALLBACK (filter_extended_events),
-      NULL);
-    g_signal_connect (
-      GET_COMPONENT("toolbarPen"),
-      "event", G_CALLBACK (filter_extended_events),
-      NULL);
-    g_signal_connect (
-      GET_COMPONENT("statusbar"),
-      "event", G_CALLBACK (filter_extended_events),
-      NULL);
-    g_signal_connect (
-      (gpointer)(gtk_scrolled_window_get_vscrollbar(GTK_SCROLLED_WINDOW(w))),
-      "event", G_CALLBACK (filter_extended_events),
-      NULL);
-    g_signal_connect (
-      (gpointer)(gtk_scrolled_window_get_hscrollbar(GTK_SCROLLED_WINDOW(w))),
-      "event", G_CALLBACK (filter_extended_events),
-      NULL);
-  }
-
-  // load the MRU
-  
-  init_mru();
 
   // and finally, open a file specified on the command line
   // (moved here because display parameters weren't initialized yet...)
   
   if (argc == 1) return;
-  set_cursor_busy(TRUE);
   if (g_path_is_absolute(argv[1]))
     tmpfn = g_strdup(argv[1]);
   else {
@@ -306,7 +141,6 @@ void init_stuff (int argc, char *argv[])
   }
   success = open_journal(tmpfn);
   g_free(tmpfn);
-  set_cursor_busy(FALSE);
   if (!success) {
     w = gtk_message_dialog_new(GTK_WINDOW (winMain), GTK_DIALOG_DESTROY_WITH_PARENT,
        GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, _("Error opening file '%s'"), argv[1]);
@@ -319,8 +153,6 @@ void init_stuff (int argc, char *argv[])
 int
 main (int argc, char *argv[])
 {
-  gchar *path, *path1, *path2;
-  
 #ifdef ENABLE_NLS
   bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
@@ -330,17 +162,6 @@ main (int argc, char *argv[])
   gtk_set_locale ();
   gtk_init (&argc, &argv);
 
-  add_pixmap_directory (PACKAGE_DATA_DIR "/" PACKAGE "/pixmaps");
-  path = g_path_get_dirname(argv[0]);
-  path1 = g_build_filename(path, "pixmaps", NULL);
-  path2 = g_build_filename(path, "..", "pixmaps", NULL);
-  add_pixmap_directory (path1);
-  add_pixmap_directory (path2);
-  add_pixmap_directory (path);
-  g_free(path);
-  g_free(path1);
-  g_free(path2);
-
   /*
    * The following code was added by Glade to create one of each component
    * (except popup menus), just so that you see something after building
@@ -349,14 +170,10 @@ main (int argc, char *argv[])
   winMain = create_winMain ();
   
   init_stuff (argc, argv);
-  gtk_window_set_icon(GTK_WINDOW(winMain), create_pixbuf("xournal.png"));
   print_to_pdf(argv[2]);
 //gtk_main ();
   
   if (bgpdf.status != STATUS_NOT_INIT) shutdown_bgpdf();
-
-  save_mru_list();
-  if (ui.auto_save_prefs) save_config_to_file();
   
   return 0;
 }
